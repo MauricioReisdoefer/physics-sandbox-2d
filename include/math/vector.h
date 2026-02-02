1701b5de3357a3088b2325525ecb7a1fe0ef2ee1
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-02-02 02:38:37 -0300

    feat: creating Vector headers

--- include/math/vector.h
+++ include/math/vector.h
@@ -3,8 +3,19 @@
 
 typedef struct Vector
 {
-    int x;
-    int y;
+    double x;
+    double y;
 } Vector;
+
+Vector *Vector_Create(double x, double y);
+void Vector_Destroy();
+
+Vector *Vector_Product(Vector *a, Vector *b);
+Vector *Vector_Sum(Vector *a, Vector *b);
+Vector *Vector_Subtraction(Vector *a, Vector *b);
+Vector *Vector_Division(Vector *a, Vector *b);
+
+Vector *Vector_Product_Double(Vector *vector, double x);
+Vector *Vector_Product_Division(Vector *vector, double x);
 
 #endif
