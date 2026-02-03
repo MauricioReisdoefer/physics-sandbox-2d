3f5605351efe9389743b2b1634d838a4a1c04350
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-02-02 22:08:25 -0300

    feat: transform module header

---
+++ include/module/transform.h
@@ -0,0 +1,18 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+#include "module.h"
+#include "vector.h"
+
+typedef struct Transform
+{
+    Module *base;
+    Vector position;
+    Vector scale;
+    double rotation;
+} Transform;
+
+Transform *Transform_Create(Vector position, Vector scale, double rotation);
+void Transform_Destroy(Transform *self);
+
+#endif
